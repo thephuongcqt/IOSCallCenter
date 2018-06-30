503675fb57028a8f3e575234b9e13ce535fd3c2d
Nguyen The Phuong <thephuongcqt@gmail.com>
2018-06-30 17:03:40 +0700

    Done Load appointment list

--- CallCenter/Controller/HomeController.swift
+++ CallCenter/Controller/HomeController.swift
@@ -1,30 +1,28 @@
 //
-//  NavigationViewController.swift
+//  HomeController.swift
 //  CallCenter
 //
-//  Created by Nguyen The Phuong on 6/25/18.
+//  Created by Nguyen The Phuong on 6/29/18.
 //  Copyright © 2018 Nguyen The Phuong. All rights reserved.
 //
 
 import UIKit
 
-class NavigationViewController: UINavigationController {
+class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
-    }    
+        // Do any additional setup after loading the view.
+    }
+    
+
     /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
+        // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
     */
---
+++ CallCenter/View/AppointmentTableHeader.swift
@@ -0,0 +1,21 @@
+//
+//  AppointmentTableHeader.swift
+//  CallCenter
+//
+//  Created by Nguyen The Phuong on 6/30/18.
+//  Copyright © 2018 Nguyen The Phuong. All rights reserved.
+//
+
+import UIKit
+
+class AppointmentTableHeader: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
